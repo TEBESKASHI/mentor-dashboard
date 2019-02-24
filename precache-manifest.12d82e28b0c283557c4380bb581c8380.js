15cc96b649facbc28bcae826d1c568509c7e074e
Maks Grickevich <maks.grickevich@mail.ru>
2019-02-25 00:01:20 +0300

    feat:add json, create react component for dashboard and color indicate

---
+++ precache-manifest.12d82e28b0c283557c4380bb581c8380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d8981764ef3516a8ba8a",
+    "url": "/static/js/main.d8981764.chunk.js"
+  },
+  {
+    "revision": "f28c1dc94fd254ffbc18",
+    "url": "/static/js/2.f28c1dc9.chunk.js"
+  },
+  {
+    "revision": "d8981764ef3516a8ba8a",
+    "url": "/static/css/main.cbfa3bd8.chunk.css"
+  },
+  {
+    "revision": "3a41e43ca2c31630cfab2f01bef83d3f",
+    "url": "/index.html"
+  }
+];
